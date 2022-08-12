aa300122ac4ddaa739b39d87f72a1324be250541
Alex Molchanov <amol4anov@mail.ru>
2022-08-12 15:17:49 +0300

    autostart patch

--- config.def.h
+++ config.def.h
@@ -51,6 +51,13 @@ static const int repeat_delay = 600;
 static const int tap_to_click = 1;
 static const int natural_scrolling = 0;
 
+/* Autostart */
+static const char *const autostart[] = {
+	    // example
+        // "sh", "-c", "swaybg --image /xap/local/background", NULL,
+        NULL /* terminate */
+};
+
 /* If you want to use the windows key change this to WLR_MODIFIER_LOGO */
 #define MODKEY WLR_MODIFIER_ALT
 #define TAGKEYS(KEY,SKEY,TAG) \
